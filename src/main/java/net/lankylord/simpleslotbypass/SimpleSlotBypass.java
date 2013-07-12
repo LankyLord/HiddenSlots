e4e4a3b82711a9e7bbc82e0d2f48f2ed3aaa9819
LankyLord <scottkendall1@gmail.com>
2013-07-12 23:00:35 +0100

    Initial Commit

---
+++ src/main/java/net/lankylord/simpleslotbypass/SimpleSlotBypass.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2013, LankyLord
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.lankylord.simpleslotbypass;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import net.lankylord.simpleslotbypass.listeners.LoginListener;
+import net.lankylord.simpleslotbypass.listeners.PingListener;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.mcstats.MetricsLite;
+
+/**
+ *
+ * @author LankyLord
+ */
+public class SimpleSlotBypass extends JavaPlugin {
+
+    static final Logger logger = Logger.getLogger("Minecraft");
+    public int publicSlots;
+    public int maximumSlots;
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        saveConfig();
+        loadMetrics();
+        publicSlots = getConfig().getInt("public-slot-count");
+        maximumSlots = (getConfig().getInt("public-slot-count") + getConfig().getInt("reserved-slot-count"));
+        registerListeners();
+    }
+
+    private void loadMetrics() {
+        try {
+            MetricsLite metrics = new MetricsLite(this);
+            metrics.start();
+        } catch (IOException e) {
+            logger.log(Level.WARNING, "Failed to submit stats");
+        }
+    }
+
+    private void registerListeners() {
+        getServer().getPluginManager().registerEvents(new LoginListener(this), this);
+        getServer().getPluginManager().registerEvents(new PingListener(this), this);
+    }
+}
